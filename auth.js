246c7678a2fe4a0d06a491e07c75c9fc27a526a3
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2019-04-29 18:37:45 +0800

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+    user: "username",
+    password: "password",
+    db: "ffmpeg",
+    dbuser: "ffmpeg",
+    dbpassword: "dbpassword",
+    secret: "yoursecret",
+    login: "/admin",
+    loginmsg: "404 Not Found"
+};
